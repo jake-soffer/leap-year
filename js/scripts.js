01762f4292cf03faa04b9f423226e8c3d5effff9
jbs <soffer94@gmail.com>
2018-03-05 12:17:19 -0800

    Implement updated business and ui logic.

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,15 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#results").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("")
+    }
+
+    $("#results").show();
   });
 });
